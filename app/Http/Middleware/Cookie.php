0fb33b0c95b2fb8d84dd3713276f052401b5692d
ACVer <2416582044@qq.com>
2019-04-14 19:08:27 +0800

    ()

--- app/Http/Middleware/Cookie.php
+++ app/Http/Middleware/Cookie.php
@@ -16,14 +16,14 @@ class Cookie
     public function handle($request, Closure $next)
     {
         $response = $next($request);
-        $origin = $request->server('HTTP_ORIGIN') ?: '';
+        $origin = $request->server('HTTP_ORIGIN') ?: '*';
 //        $allow_origin = [
 //            'http://localhost',
 //            'http://127.0.0.1',
 //        ];
         $response->headers->set('Access-Control-Allow-Origin', '*');
         if (env('APP_ENV') != 'production') {
-//            $response->headers->set('Access-Control-Allow-Origin', $origin);
+            $response->headers->set('Access-Control-Allow-Origin', $origin);
             $response->headers->set('Access-Control-Allow-Headers', 'Origin, Content-Type, Cookie, Accept, Authorization');
             $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, OPTIONS, DELETE');
             $response->headers->set('Access-Control-Allow-Credentials', 'true');
